24cc9bc78d40580088fb1eefa71e16f330d3abe7
pmop <pmop@ic.ufal.br>
2020-03-03 19:16:48 -0300

    Adding prime checking function to utils.

--- utils.js
+++ utils.js
@@ -14,3 +14,21 @@ function tail(array) {
 function end(array) {
 	  return array.length-1;
 }
+
+
+// Prime checking using classic squareroot method
+function isPrime(num) {
+	let index = num-1;
+	const table = [false,true,true,false,true,false,true,false,false,false];
+	if (index < table.length) return table[index];
+	else {
+		let prime = true;
+		for(let i = 2; (i <= ~~(Math.sqrt(num))) && prime; ++i) {
+			if (num%i === 0) prime = false;
+		}
+		return prime;
+
+	}
+}
+
+
